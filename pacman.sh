a6458d13a9ec56a4e4dfcbd59838ea3efc1d73f0
Wouter Wijsman <wwijsman@live.nl>
2020-05-08 15:19:33 +0200

    Only create build dir if needed

--- pacman.sh
+++ pacman.sh
@@ -3,6 +3,7 @@
 
 # Exit on errors
 set -e
+set -u
 
 ## Remove $CC and $CXX for configure
 unset CC
@@ -11,8 +12,8 @@ unset CXX
 ## Enter the psp-pacman directory.
 cd "$(dirname "$0")"
 
+## Load common functions
 source common.sh
-mkdir -p build
 
 ## Variables used to build
 PACMAN_VERSION="5.2.1"
@@ -21,6 +22,7 @@ BASE_PATH="${PWD}"
 
 ## Only install if pacman is not available
 if ! which "pacman" >/dev/null 2>&1; then
+    mkdir -p "${BASE_PATH}/build"
     cd "${BASE_PATH}/build"
     download_and_extract https://sources.archlinux.org/other/pacman/pacman-${PACMAN_VERSION}.tar.gz pacman-${PACMAN_VERSION}
     apply_patch pacman-${PACMAN_VERSION}
