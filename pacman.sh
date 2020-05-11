f304499e2be39f7d22b4fe106975ce7eaef24019
Wouter Wijsman <wwijsman@live.nl>
2020-05-11 12:23:36 +0200

    Install in $PSPDEV and don't require patch
    There is still an issue with bash-completion which makes the
    installation fail without root permission. I'll work on this.

--- pacman.sh
+++ pacman.sh
@@ -25,14 +25,14 @@ if ! which "pacman" >/dev/null 2>&1; then
     mkdir -p "${BASE_PATH}/build"
     cd "${BASE_PATH}/build"
     download_and_extract https://sources.archlinux.org/other/pacman/pacman-${PACMAN_VERSION}.tar.gz pacman-${PACMAN_VERSION}
-    apply_patch pacman-${PACMAN_VERSION}
+    #apply_patch pacman-${PACMAN_VERSION}
 
     ## Install meson and ninja in the current directory
     setup_build_system
 
     ## Build pacman
     meson build
-    meson configure build -Dprefix=${INSTALL_DIR} -Dbuildscript=PSPBUILD -Droot-dir=${PSPDEV} -Ddoc=disabled -Dbash-completion=false
+    meson configure build -Dprefix="${PSPDEV}" -Dbuildscript=PSPBUILD -Droot-dir="${PSPDEV}" -Dbindir="${PSPDEV}/share/pacman/bin" -Ddoc=disabled
     cd build
     ninja
 
@@ -42,13 +42,13 @@ fi
 
 ## Install configuration files and wrapper script
 cd "${BASE_PATH}"
-install -D -m 644 config/pacman.conf "${INSTALL_DIR}/etc/pacman.conf"
-install -D -m 644 config/makepkg.conf "${INSTALL_DIR}/etc/makepkg.conf"
+install -D -m 644 config/pacman.conf "${PSPDEV}/etc/pacman.conf"
+install -D -m 644 config/makepkg.conf "${PSPDEV}/etc/makepkg.conf"
 install -D -m 755 scripts/psp-pacman "${PSPDEV}/bin/psp-pacman"
 install -D -m 755 scripts/psp-makepkg "${PSPDEV}/bin/psp-makepkg"
 
 ## Make sure the dbpath directory exists
-mkdir -p "${INSTALL_DIR}/var/lib/pacman"
+mkdir -p "${PSPDEV}/var/lib/pacman"
 
 ## Done
 echo "Installation finished."
